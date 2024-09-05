0e7a9783dcd05e9797d1ed46c302bad56e381bbc
339743478 <339743478@2260I-L29-002>
2024-09-05 09:29:56 -0400

    Add project files.

---
+++ Topic 1, The Basics/Program.cs
@@ -0,0 +1,15 @@
+﻿// I am invisible >:D
+Console.WriteLine("Hello, World!");
+Console.WriteLine("press ENTER to continue");
+Console.WriteLine("or you will explode!!!");
+// Console.WriteLine("what the sigma");
+Console.ReadLine();
+Console.WriteLine("awesome epic");
+// IGNORE THIS THIS ISN'T IMPORTANT
+Console.WriteLine("Yesterday was Thursday, today is Armageddon");
+Console.WriteLine("why am I doing this aaaaaaaaaaaaa");
+Console.ReadLine();
+Console.WriteLine("");
+Console.WriteLine("stop it!");
+Console.ReadLine();
+Console.WriteLine("bro");
